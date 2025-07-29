aa7c1c42eef18139b9fac511e5dfe821d9d9e329
andronov5 <lerajanecutie@gmail.com>
2025-07-29 08:28:38 -0600

    Add get_max_profit helper and tests

--- model/__init__.py
+++ model/__init__.py
@@ -1,6 +1,11 @@
-from .features import download_or_load_prices, compute_features, data_prep_and_feature_engineering
+from .features import (
+    download_or_load_prices,
+    compute_features,
+    data_prep_and_feature_engineering,
+)
 from .grid_search import run_grid_search
 from .backtest import run_backtest
+from .utils import get_max_profit
 
 __all__ = [
     "download_or_load_prices",
@@ -8,4 +13,5 @@ __all__ = [
     "data_prep_and_feature_engineering",
     "run_grid_search",
     "run_backtest",
+    "get_max_profit",
 ]
